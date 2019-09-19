1a141b07ffe3413598904b8a10e041f404cafd58
Dillon Kearns <dillon@dillonkearns.com>
2019-09-19 11:14:00 -0700

    Add test harness for testing HTML renderers.

---
+++ tests/HtmlHandlerTests.elm
@@ -0,0 +1,89 @@
+module HtmlHandlerTests exposing (suite)
+
+import Expect exposing (Expectation)
+import Markdown.Inlines
+import Markdown.Parser as Markdown exposing (..)
+import Parser
+import Parser.Advanced as Advanced
+import Test exposing (..)
+
+
+type alias Parser a =
+    Advanced.Parser String Parser.Problem a
+
+
+parse : String -> Result (List (Advanced.DeadEnd String Parser.Problem)) (List Block)
+parse =
+    Markdown.parse
+
+
+type Rendered
+    = Unexpected String
+    | Html String
+
+
+testRenderer : List (Markdown.Decoder (List Rendered -> Rendered)) -> Markdown.Renderer Rendered
+testRenderer htmlHandlers =
+    { heading =
+        \{ level, children } ->
+            Unexpected "heading"
+    , raw = \_ -> Unexpected "String"
+    , bold =
+        \content -> Unexpected "String"
+    , italic =
+        \content -> Unexpected "String"
+    , code =
+        \content -> Unexpected "String"
+    , link =
+        \link content ->
+            Unexpected "String"
+                |> Ok
+    , plain =
+        \_ ->
+            Unexpected "String"
+    , list =
+        \items ->
+            Unexpected "String"
+    , htmlDecoder = Markdown.htmlOneOf htmlHandlers
+    , codeBlock =
+        \{ body, language } ->
+            Unexpected "String"
+    , thematicBreak = Unexpected "String"
+    }
+
+
+suite : Test
+suite =
+    describe "html handlers"
+        [ test "basic html tag" <|
+            \() ->
+                "<social-links />"
+                    |> Markdown.render
+                        (testRenderer
+                            [ Markdown.htmlTag "social-links" (\children -> Html "social-links")
+                            ]
+                        )
+                    |> Expect.equal (Ok [ Html "social-links" ])
+        ]
+
+
+unstyledTextSingle : String -> Markdown.Inlines.StyledString
+unstyledTextSingle body =
+    { string = body
+    , style =
+        { isCode = False
+        , isBold = False
+        , isItalic = False
+        , link = Nothing
+        }
+    }
+
+
+parserError : String -> Expect.Expectation
+parserError markdown =
+    case parse markdown of
+        Ok _ ->
+            Expect.fail "Expected a parser failure!"
+
+        Err _ ->
+            Expect.pass
