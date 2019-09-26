18505d4e88aeea9fffd2e29ed314185cbb20259b
Dillon Kearns <dillon@dillonkearns.com>
2019-09-25 21:32:17 -0700

    Save passing test cases under suite name json key.

--- test/specs/run-spec.js
+++ test/specs/run-spec.js
@@ -22,6 +22,7 @@ function runSpecs(title, dir, showCompletionTable, options) {
         specs[section].specs.forEach(spec => {
           spec.options = Object.assign({}, options, spec.options || {});
           const example = spec.example ? " example " + spec.example : "";
+          spec.suiteTitle = title;
           const passFail = spec.shouldFail ? "fail" : "pass";
           if (typeof spec.options.silent === "undefined") {
             spec.options.silent = true;
@@ -74,7 +75,7 @@ runSpecs("ReDOS", "./redos");
 runSpecs("Security", "./security", false, { silent: true }); // silent - do not show deprecation warning
 
 function printStatus() {
-  /** @typedef { { markdown: string; html: string; example: number; start_line: number; end_line: number; section: string; options: { gfm: boolean; pedantic: boolean; headerIds: boolean; silent: boolean; }; } } Spec */
+  /** @typedef { { markdown: string; suiteTitle: string; html: string; example: number; start_line: number; end_line: number; section: string; options: { gfm: boolean; pedantic: boolean; headerIds: boolean; silent: boolean; }; } } Spec */
   // @ts-ignore
   /** @type {Spec[]]} */ let failures = global.fails;
   // @ts-ignore
@@ -83,15 +84,19 @@ function printStatus() {
     /** @type {Object} */ accumulator,
     spec
   ) {
-    accumulator[spec.section] = accumulator[spec.section] || [];
-    accumulator[spec.section].push(spec.example);
+    accumulator[spec.suiteTitle] = accumulator[spec.suiteTitle] || {};
+    accumulator[spec.suiteTitle][spec.section] =
+      accumulator[spec.suiteTitle][spec.section] || [];
+    accumulator[spec.suiteTitle][spec.section].push(spec.example);
 
     // Pass the object on to the next loop
     return accumulator;
   },
   {});
-  Object.keys(passedJson).forEach(section => {
-    passedJson[section].sort();
+  Object.keys(passedJson).forEach(suiteTitle => {
+    Object.keys(passedJson[suiteTitle]).forEach(section => {
+      passedJson[suiteTitle][section].sort();
+    });
   });
   fs.writeFileSync("./spec-results.json", JSON.stringify(passedJson));
 }
