9d934454b1d0e17fb6d7e50af58bc8343d034c56
Dillon Kearns <dillon@dillonkearns.com>
2019-09-29 09:05:44 -0700

    Add elm.json for elm package and add some docs.

--- src/Markdown/Block.elm
+++ src/Markdown/Block.elm
@@ -1,5 +1,11 @@
 module Markdown.Block exposing (Block(..))
 
+{-|
+
+@docs Block
+
+-}
+
 import Markdown.CodeBlock
 import Markdown.Inlines as Inlines exposing (StyledString)
 
@@ -8,6 +14,34 @@ type alias Attribute =
     { name : String, value : String }
 
 
+{-| This is the AST (abstract syntax tree) that represents your parsed markdown.
+
+In the simplest case, you can pass this directly to a renderer:
+
+    module Main exposing (main)
+
+    import Markdown.Block exposing (Block)
+    import Markdown.Parser
+
+    markdown : String
+    markdown =
+        "# This is a title!\n\nThis is the body."
+
+    astResult : Result (List (Advanced.DeadEnd String Parser.Problem)) (List Block)
+    astResult =
+        markdown
+            |> Markdown.Parser.parse
+
+    main : Html msg
+    main =
+        case astResult of
+            Ok ast ->
+                Markdown.Parser.renderDefaultHtml ast
+
+            Err errors ->
+                Html.text "Encountered parsing errors."
+
+-}
 type Block
     = Heading Int (List StyledString)
     | Body (List StyledString)
