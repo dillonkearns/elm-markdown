9c9a1cd74fc9b2c277c7eb736691ae3283aea8cd
Dillon Kearns <dillon@dillonkearns.com>
2019-10-05 08:33:18 -0700

    Update example to use optional attribute.

--- examples/src/CustomHtmlBlockSignupForm.elm
+++ examples/src/CustomHtmlBlockSignupForm.elm
@@ -36,24 +36,33 @@ markdownBody =
 You just render it like this
 
 ```
-<bio name="Dillon Kearns" photo="https://avatars2.githubusercontent.com/u/1384166">
+<bio
+  name="Dillon Kearns"
+  photo="https://avatars2.githubusercontent.com/u/1384166"
+  twitter="dillontkearns"
+  github="dillonkearns"
+>
 Dillon really likes building things with Elm! Here are some links
 
-- [Github](https://github.com/dillonkearns/)
-- [Twitter](https://twitter.com/dillontkearns/)
 - [Articles](https://incrementalelm.com/articles)
 </bio>
 ```
 
 And you get a custom view like this!
 
-<bio name="Dillon Kearns" photo="https://avatars2.githubusercontent.com/u/1384166">
+<bio
+  name="Dillon Kearns"
+  photo="https://avatars2.githubusercontent.com/u/1384166"
+  twitter="dillontkearns"
+  github="dillonkearns"
+>
 Dillon really likes building things with Elm! Here are some links
 
-- [Github](https://github.com/dillonkearns/)
-- [Twitter](https://twitter.com/dillontkearns/)
 - [Articles](https://incrementalelm.com/articles)
 </bio>
+
+Note that these attributes are all optional. Try removing them and see what happens!
+Or you can add `dribbble="something"` and see that icon show up if it's provided.
 """
 
 
@@ -150,14 +159,20 @@ renderer =
     , codeBlock = codeBlock
     , html =
         Markdown.Html.oneOf
-            [ Markdown.Html.tag "bio" (\name photoUrl renderedChildren -> bioView renderedChildren name photoUrl)
+            [ Markdown.Html.tag "bio"
+                (\name photoUrl twitter github dribbble renderedChildren ->
+                    bioView renderedChildren name photoUrl twitter github dribbble
+                )
                 |> Markdown.Html.withAttribute "name"
                 |> Markdown.Html.withAttribute "photo"
+                |> Markdown.Html.withOptionalAttribute "twitter"
+                |> Markdown.Html.withOptionalAttribute "github"
+                |> Markdown.Html.withOptionalAttribute "dribbble"
             ]
     }
 
 
-bioView renderedChildren name photoUrl =
+bioView renderedChildren name photoUrl twitter github dribbble =
     Element.column
         [ Element.Border.shadow
             { offset = ( 0.3, 0.3 )
@@ -170,13 +185,50 @@ bioView renderedChildren name photoUrl =
         , Element.centerX
         , Font.center
         ]
-        (avatarView photoUrl
-            :: Element.el
+        (Element.row [ Element.spacing 20 ]
+            [ avatarView photoUrl
+            , Element.el
                 [ Font.bold
                 , Font.size 30
                 ]
                 (Element.text name)
+            , icons twitter github dribbble
+            ]
             :: renderedChildren
+        )
+
+
+icons twitter github dribbble =
+    Element.row []
+        ([ twitter
+            |> Maybe.map
+                (\twitterHandle ->
+                    Element.link []
+                        { url = "https://twitter.com/" ++ twitterHandle
+                        , label =
+                            Element.image [] { src = "https://i.imgur.com/tXSoThF.png", description = "Twitter Logo" }
+                        }
+                )
+         , github
+            |> Maybe.map
+                (\twitterHandle ->
+                    Element.link []
+                        { url = "https://github.com/" ++ twitterHandle
+                        , label =
+                            Element.image [] { src = "https://i.imgur.com/0o48UoR.png", description = "Github Logo" }
+                        }
+                )
+         , dribbble
+            |> Maybe.map
+                (\dribbbleHandle ->
+                    Element.link []
+                        { url = "https://dribbble.com/" ++ dribbbleHandle
+                        , label =
+                            Element.image [] { src = "https://i.imgur.com/1AGmwO3.png", description = "Dribbble Logo" }
+                        }
+                )
+         ]
+            |> List.filterMap identity
         )
 
 
