06d8efd41fbff698b078da0adfa554758b05d6ed
Dillon Kearns <dillon@dillonkearns.com>
2019-10-04 20:03:42 -0700

    Add basic elm-ui rendering example.

---
+++ examples/src/ElmUi.elm
@@ -0,0 +1,258 @@
+module ElmUi exposing (main)
+
+import Element exposing (Element)
+import Element.Background
+import Element.Border
+import Element.Font as Font
+import Element.Region
+import Html exposing (Attribute, Html)
+import Html.Attributes
+import Markdown.Block exposing (Block, Inline, InlineStyle)
+import Markdown.Html
+import Markdown.Parser
+
+
+main : Html msg
+main =
+    Element.layout []
+        (case view markdownBody of
+            Ok rendered ->
+                Element.column
+                    [ Element.spacing 30
+                    , Element.padding 50
+                    ]
+                    rendered
+
+            Err errors ->
+                Element.text errors
+        )
+
+
+markdownBody =
+    """
+# Esse intrata referre inter adspeximus aequora soror
+
+## Ebur iamque mecum
+
+Lorem markdownum [vitae](http://minantia.io/herbis-caelumque.aspx) crines
+carminibus exponit pugnax dilectaque Sparte te est. Nullos imperium! Ait qui
+corpora perstat Gryneus fidem iunctura. Hic sperne inquit iuventus timidasque
+iuvenis stirpe barbarus sorori? Fatebor non in iaculatur, concuteret auget
+corpore accepere vectus pacisci quoque renascitur essem: frugum labentibus
+Naxoque festum despectat.
+
+## Saepe nec tergo Iole te solent pharetras
+
+Iamque insula, ore longe dixerat libratum neque terrarum resedit de iuranda cum
+muneris *tamen*, suas populique te. Alumno invidiae cecinit exarsit modo vidit
+ingentia suum, et pluribus sensu *Danais* adigitque acervo gravis visae,
+capillos!
+
+```
+var post = client_script_agp(bar_address_golden(flash, digital_halftone_unix
+        + 3), supercomputer_bridge);
+type_ctr_drag(waveform, -2, core_rom);
+if (frameworkFile * wiki_ddr >= architecture_core(station_mebibyte, 5,
+        dual_cd)) {
+    tft.logMirror(infotainmentPram + spoofingArchitectureServer,
+            sliRiscParse + mmsClean);
+}
+```
+
+## Vetustas caede
+
+Grata raucaque dixit delenda terris. Actorum circumdata fronde fuerat, accepisse
+certe, haurit manu. [Ventura Achille](http://www.devolenti.net/canoro.php)
+admovit, non ut tempto violas est ego pater; fit probavit iaculi Ophiusiaque
+inque. Conlectae est premebat subsunt. Dum ad adusque sol sub vini, quod: per
+sanguine, recludit posuisti: Trinacris Sibyllae.
+
+## Tamquam novus
+
+Nec munera pia sequuntur consedit est vultus, **enim laeva**, hortaturque
+sulphura fraterna somni [circumstantes](http://illis-vacant.io/susmater.php)?
+Futurae habet visa cogit natus coeperat lacertos luxuriem, coloribus quaecumque
+unus membra? Et molirique saevior terrae concubiturus pars. Aequor convivia ergo
+nec salutem, absentem veris exspirat, traxit deiectoque dedignata evolat pressit
+me promissa amor ardor.
+
+Iuvenem fluunt populusque iterum arcet donavi testatos tellus semperque
+debueram. Edidit [illo votis](http://sum.net/cumfelixque) Melicerta *vivus*,
+mare, **praefoderat iubasque**? Dabat **temerarius boves orbe** populi!
+
+## Sic colitur tecum exsultantemque fessis vidit rescindere
+
+Polydoreo Iovis mentis fratre posse, claudit placabilis nisi radiante premunt,
+cum committitur, inquit bovem caput, vocem! De sensit vestigia super. Effugit
+nux tamen nota pererrat nec semel erat: quater e solvi non nec **inmitem
+tristi**.
+
+Talia litore glomerataque quantum lentaque **restat**, nec lapsa Threiciis
+subiere tamen exercere et tuis. Est fine cum supposito iamque, ex templa illa
+cursus venerit tenebat et? Quemque mihi, dare erudit Lyncus, ab dicebar iterum
+exanimi sermone; esse Iunonem paelicis mundi velit.
+
+*Tydiden dubitabile neque* conscendere ardor verboque sic refert Auroram
+sequantur praemia doleam pectusque fumantia hospes, cum silvaque caputque. Domat
+et annis corpus est aperire amoris. Concha non quae columbas, quae tenuem,
+pervia, euntis?
+"""
+
+
+buildToc : List Block -> TableOfContents
+buildToc blocks =
+    let
+        headings =
+            gatherHeadings blocks
+    in
+    headings
+        |> List.map Tuple.second
+        |> List.map
+            (\styledList ->
+                { anchorId = styledToString styledList |> rawTextToId
+                , name = styledToString styledList
+                , level = 1
+                }
+            )
+
+
+styledToString : List Inline -> String
+styledToString list =
+    List.map .string list
+        |> String.join "-"
+
+
+gatherHeadings : List Block -> List ( Int, List Inline )
+gatherHeadings blocks =
+    List.filterMap
+        (\block ->
+            case block of
+                Markdown.Block.Heading level content ->
+                    Just ( level, content )
+
+                _ ->
+                    Nothing
+        )
+        blocks
+
+
+type alias TableOfContents =
+    List { anchorId : String, name : String, level : Int }
+
+
+view : String -> Result String (List (Element msg))
+view markdown =
+    markdown
+        |> Markdown.Parser.parse
+        |> Result.mapError (\error -> error |> List.map Markdown.Parser.deadEndToString |> String.join "\n")
+        |> Result.andThen (Markdown.Parser.render renderer)
+
+
+renderer : Markdown.Parser.Renderer (Element msg)
+renderer =
+    { heading = heading
+    , raw =
+        Element.paragraph
+            [ Element.spacing 15 ]
+    , thematicBreak = Element.none
+    , plain = Element.text
+    , bold = \content -> Element.row [ Font.bold ] [ Element.text content ]
+    , italic = \content -> Element.row [ Font.italic ] [ Element.text content ]
+    , code = code
+    , link =
+        \{ title, destination } body ->
+            Element.link
+                [ Element.htmlAttribute (Html.Attributes.style "display" "inline-flex") ]
+                { url = destination
+                , label =
+                    Element.paragraph
+                        [ Font.color (Element.rgb255 0 0 255)
+                        ]
+                        body
+                }
+                |> Ok
+    , image =
+        \image body ->
+            Element.image [ Element.width Element.fill ] { src = image.src, description = body }
+                |> Ok
+    , list =
+        \items ->
+            Element.column [ Element.spacing 15 ]
+                (items
+                    |> List.map
+                        (\itemBlocks ->
+                            Element.row [ Element.spacing 5 ]
+                                [ Element.el
+                                    [ Element.alignTop ]
+                                    (Element.text "•")
+                                , itemBlocks
+                                ]
+                        )
+                )
+    , codeBlock = codeBlock
+    , html = Markdown.Html.oneOf []
+    }
+
+
+rawTextToId rawText =
+    rawText
+        |> String.toLower
+        |> String.replace " " ""
+
+
+heading : { level : Int, rawText : String, children : List (Element msg) } -> Element msg
+heading { level, rawText, children } =
+    Element.paragraph
+        [ Font.size
+            (case level of
+                1 ->
+                    36
+
+                2 ->
+                    24
+
+                _ ->
+                    20
+            )
+        , Font.bold
+        , Font.family [ Font.typeface "Montserrat" ]
+        , Element.Region.heading level
+        , Element.htmlAttribute
+            (Html.Attributes.attribute "name" (rawTextToId rawText))
+        , Element.htmlAttribute
+            (Html.Attributes.id (rawTextToId rawText))
+        ]
+        children
+
+
+code : String -> Element msg
+code snippet =
+    Element.el
+        [ Element.Background.color
+            (Element.rgba 0 0 0 0.04)
+        , Element.Border.rounded 2
+        , Element.paddingXY 5 3
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=Source+Code+Pro"
+                , name = "Source Code Pro"
+                }
+            ]
+        ]
+        (Element.text snippet)
+
+
+codeBlock : { body : String, language : Maybe String } -> Element msg
+codeBlock details =
+    Element.el
+        [ Element.Background.color (Element.rgba 0 0 0 0.03)
+        , Element.htmlAttribute (Html.Attributes.style "white-space" "pre")
+        , Element.padding 20
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=Source+Code+Pro"
+                , name = "Source Code Pro"
+                }
+            ]
+        ]
+        (Element.text details.body)
