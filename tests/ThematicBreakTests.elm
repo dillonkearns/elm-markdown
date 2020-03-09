aa7643371e4a21be82943f8b47d9e5ba3dde9f36
Dillon Kearns <dillon@dillonkearns.com>
2020-03-09 12:17:21 -0700

    Get basic parsing working for thematic breaks.

--- tests/ThematicBreakTests.elm
+++ tests/ThematicBreakTests.elm
@@ -27,15 +27,64 @@ type State
     | NoMatchYet
 
 
+type ThematicToken
+    = Star
+    | Dash
+    | Finished
+
+
 statementsHelp : State -> Parser (Advanced.Step State ThematicBreak)
 statementsHelp state =
-    succeed identity
-        |. oneOf
-            [ tokenHelp "-"
-            , tokenHelp "*"
-            ]
-        |= Advanced.succeed
-            (Advanced.Done ThematicBreak)
+    oneOf
+        [ tokenHelp "-" |> map (\_ -> Dash)
+        , tokenHelp "*" |> map (\_ -> Star)
+        , end (Parser.Expecting "end") |> map (\_ -> Finished)
+        , tokenHelp "\n" |> map (\_ -> Finished)
+        ]
+        |> andThen
+            (\thematicToken ->
+                case ( thematicToken, state ) of
+                    ( Finished, NoMatchYet ) ->
+                        problem (Parser.Expecting "TODO")
+
+                    ( Finished, Asterisk occurrences ) ->
+                        succeedIfEnough occurrences
+
+                    ( Finished, Hyphen occurrences ) ->
+                        succeedIfEnough occurrences
+
+                    ( Star, Asterisk occurrences ) ->
+                        Advanced.Loop (Asterisk (occurrences + 1))
+                            |> succeed
+
+                    ( Dash, Hyphen occurrences ) ->
+                        Advanced.Loop (Hyphen (occurrences + 1))
+                            |> succeed
+
+                    ( Star, NoMatchYet ) ->
+                        Advanced.Loop (Asterisk 1)
+                            |> succeed
+
+                    ( Dash, NoMatchYet ) ->
+                        Advanced.Loop (Hyphen 1)
+                            |> succeed
+
+                    _ ->
+                        problem (Parser.Expecting "TODO")
+            )
+
+
+succeedIfEnough occurences =
+    if occurences > 2 then
+        succeed (Done ThematicBreak)
+
+    else
+        problem (Parser.Expecting "...?")
+
+
+
+--|= Advanced.succeed
+--    (Advanced.Done ThematicBreak)
 
 
 tokenHelp char =
