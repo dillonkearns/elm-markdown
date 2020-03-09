60474c95bbd516ce28b015b2e2809f33bd39390c
Dillon Kearns <dillon@dillonkearns.com>
2020-03-09 12:04:11 -0700

    Update parsing for thematic breaks.

--- tests/ThematicBreakTests.elm
+++ tests/ThematicBreakTests.elm
@@ -4,7 +4,7 @@ import Expect exposing (Expectation)
 import Markdown.ListItem exposing (Completion(..), ListItem(..))
 import Markdown.Parser
 import Parser
-import Parser.Advanced as Advanced
+import Parser.Advanced as Advanced exposing (..)
 import Test exposing (..)
 
 
@@ -18,7 +18,6 @@ type ThematicBreak
 
 parser : Parser ThematicBreak
 parser =
-    --Advanced.succeed ThematicBreak
     Advanced.loop NoMatchYet statementsHelp
 
 
@@ -28,13 +27,19 @@ type State
     | NoMatchYet
 
 
-
---statementsHelp : Maybe ThematicChar -> Parser (Advanced.Step ThematicChar ThematicBreak)
-
-
 statementsHelp : State -> Parser (Advanced.Step State ThematicBreak)
-statementsHelp revStmts =
-    Advanced.succeed (Advanced.Done ThematicBreak)
+statementsHelp state =
+    succeed identity
+        |. oneOf
+            [ tokenHelp "-"
+            , tokenHelp "*"
+            ]
+        |= Advanced.succeed
+            (Advanced.Done ThematicBreak)
+
+
+tokenHelp char =
+    Advanced.token (Advanced.Token char (Parser.Expecting char))
 
 
 suite : Test
@@ -46,13 +51,16 @@ suite =
                     |> Advanced.run parser
                     |> Expect.equal
                         (Ok ThematicBreak)
-
-        --, test "asterisks" <|
-        --    \() ->
-        --        "***"
-        --            |> Advanced.run parser
-        --            |> Expect.equal
-        --                (Ok ThematicBreak)
+        , test "asterisks" <|
+            \() ->
+                "***"
+                    |> Advanced.run parser
+                    |> Expect.equal
+                        (Ok ThematicBreak)
+        , test "not enough characters" <|
+            \() ->
+                "**"
+                    |> expectFail
         , test "no thematic break characters fails" <|
             \() ->
                 "hello!"
