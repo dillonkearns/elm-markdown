f944856f7d534506dbdc7e15e0bf3e67eeeb1d8f
Dillon Kearns <dillon@dillonkearns.com>
2020-04-16 09:30:39 -0700

    Revert "Whoops, I deleted the docs comment."
    This reverts commit 864f19979b1f0f12da4c233a2d96e7fed10c3d7a.

--- src/Markdown/Block.elm
+++ src/Markdown/Block.elm
@@ -103,7 +103,8 @@ type ListItem children
     = ListItem Task (List children)
 
 
-{-| -}
+{-| A task (or no task), which may be contained in a ListItem.
+-}
 type Task
     = NoTask
     | IncompleteTask
