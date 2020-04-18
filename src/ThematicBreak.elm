5e9e0573fe43df57bcfdda38adbdbe13fd181303
Folkert <folkert@folkertdev.nl>
2020-04-18 22:48:25 +0200

    improve ThematicBreak

--- src/ThematicBreak.elm
+++ src/ThematicBreak.elm
@@ -15,90 +15,44 @@ type ThematicBreak
 
 parser : Parser ThematicBreak
 parser =
-    succeed identity
-        |. oneOf [ chompIf (\c -> c == ' ') (Parser.Expecting "Space"), succeed () ]
-        |. oneOf [ chompIf (\c -> c == ' ') (Parser.Expecting "Space"), succeed () ]
-        |. oneOf [ chompIf (\c -> c == ' ') (Parser.Expecting "Space"), succeed () ]
-        |= Advanced.loop NoMatchYet statementsHelp
-
-
-type State
-    = Asterisk Int
-    | Hyphen Int
-    | Underscore Int
-    | NoMatchYet
-
-
-type ThematicToken
-    = Star
-    | Dash
-    | TokenUnderscore
-    | Finished
-    | Whitespace
-
-
-statementsHelp : State -> Parser (Advanced.Step State ThematicBreak)
-statementsHelp state =
-    -- Investigate: would splitting this out help?
-    -- e.g. oneOf [ whenThematic, whenFinished, whenWhitespace ]
-    -- makes the case shorter, and allows `map` instead of `andThen` in some branches
+    -- a thematic break can be preceded by up to 3 spaces.
+    -- but in most documents it will be at the start of the line
+    -- so we optimize for that case
     oneOf
-        [ tokenHelp "-" |> map (\_ -> Dash)
-        , tokenHelp "*" |> map (\_ -> Star)
-        , tokenHelp "_" |> map (\_ -> TokenUnderscore)
-        , end (Parser.Expecting "end") |> map (\_ -> Finished)
-        , tokenHelp "\n" |> map (\_ -> Finished)
-        , chompIf (\c -> c == ' ') (Parser.Expecting "Space") |> map (\_ -> Whitespace)
+        [ succeed identity
+            |. chompIf isSpace (Parser.Expecting "Space")
+            |. oneOf [ chompIf isSpace (Parser.Expecting "Space"), succeed () ]
+            |. oneOf [ chompIf isSpace (Parser.Expecting "Space"), succeed () ]
+            |= parseThematicBreak
+        , parseThematicBreak
         ]
-        |> andThen
-            (\thematicToken ->
-                case ( thematicToken, state ) of
-                    ( Finished, NoMatchYet ) ->
-                        problem (Parser.Expecting "TODO")
-
-                    ( Finished, Asterisk occurrences ) ->
-                        succeedIfEnough occurrences
-
-                    ( Finished, Hyphen occurrences ) ->
-                        succeedIfEnough occurrences
-
-                    ( Finished, Underscore occurrences ) ->
-                        succeedIfEnough occurrences
-
-                    ( Star, Asterisk occurrences ) ->
-                        Advanced.Loop (Asterisk (occurrences + 1))
-                            |> succeed
-
-                    ( Dash, Hyphen occurrences ) ->
-                        Advanced.Loop (Hyphen (occurrences + 1))
-                            |> succeed
-
-                    ( TokenUnderscore, Underscore occurrences ) ->
-                        Advanced.Loop (Underscore (occurrences + 1))
-                            |> succeed
-
-                    ( TokenUnderscore, NoMatchYet ) ->
-                        Advanced.Loop (Underscore 1)
-                            |> succeed
-
-                    ( Star, NoMatchYet ) ->
-                        Advanced.Loop (Asterisk 1)
-                            |> succeed
-
-                    ( Dash, NoMatchYet ) ->
-                        Advanced.Loop (Hyphen 1)
-                            |> succeed
-
-                    ( Whitespace, _ ) ->
-                        Advanced.Loop state
-                            |> succeed
-
-                    _ ->
-                        problem (Parser.Expecting (stateToString state))
-            )
 
 
-succeedIfEnough occurences =
+type alias Occurences =
+    Int
+
+
+parseThematicBreak : Parser ThematicBreak
+parseThematicBreak =
+    oneOf
+        [ tokenHelp "-" |> andThen (\_ -> Advanced.loop 1 (statementsHelp (tokenHelp "-")))
+        , tokenHelp "*" |> andThen (\_ -> Advanced.loop 1 (statementsHelp (tokenHelp "*")))
+        , tokenHelp "_" |> andThen (\_ -> Advanced.loop 1 (statementsHelp (tokenHelp "_")))
+        ]
+
+
+statementsHelp : Parser () -> Occurences -> Parser (Advanced.Step Occurences ThematicBreak)
+statementsHelp token occurences =
+    oneOf
+        [ token |> map (\_ -> Loop (occurences + 1))
+        , whitespace occurences
+        , end (Parser.Expecting "end") |> andThen (thematicFinish occurences)
+        , tokenHelp "\n" |> andThen (thematicFinish occurences)
+        ]
+
+
+thematicFinish : Int -> a -> Parser (Step b ThematicBreak)
+thematicFinish occurences _ =
     if occurences > 2 then
         succeed (Done ThematicBreak)
 
@@ -106,17 +60,21 @@ succeedIfEnough occurences =
         problem (Parser.Expecting "...?")
 
 
-stateToString : State -> String
-stateToString state =
-    case state of
-        Asterisk v ->
-            "Asterisk " ++ String.fromInt v
+whitespace : Occurences -> Parser (Step Occurences a)
+whitespace state =
+    succeed (Advanced.Loop state)
+        |. chompIf isSpace (Parser.Expecting "Space")
+        |. chompWhile isSpace
 
-        Hyphen v ->
-            "Hyphen " ++ String.fromInt v
 
-        Underscore v ->
-            "Underscore " ++ String.fromInt v
+isSpace : Char -> Bool
+isSpace c =
+    case c of
+        ' ' ->
+            True
 
-        NoMatchYet ->
-            "NoMatchYet"
+        '\t' ->
+            True
+
+        _ ->
+            False
