9aa4b01d82049ec5338f3c3b286332016e2a9d93
Ben Burdette <>
2023-05-07 14:09:32 -0600

    defaultStringRenderer

--- src/Markdown/Renderer.elm
+++ src/Markdown/Renderer.elm
@@ -254,6 +254,123 @@ defaultHtmlRenderer =
             Html.td attrs
     }
 
+{-| This renders `Html` in an attempt to be as close as possible to
+the HTML output in <https://github.github.com/gfm/>.
+-}
+defaultStringRenderer : Renderer String
+defaultStringRenderer =
+    { heading =
+        \{ level, rawText, children } ->
+            (case level of
+                Block.H1 ->
+                    "# " ++ String.concat children
+
+                Block.H2 ->
+                    "## " ++ String.concat children
+
+                Block.H3 ->
+                    "### " ++ String.concat children
+
+                Block.H4 ->
+                    "#### " ++ String.concat children
+
+                Block.H5 ->
+                    "##### " ++ String.concat children
+
+                Block.H6 ->
+                    "###### "
+                        ++ String.concat children
+            )
+                ++ "\n\n"
+    , paragraph =
+        \strs ->
+            String.concat strs
+                ++ "\n\n"
+    , hardLineBreak = "  \n"
+    , blockQuote =
+        \strs ->
+            strs
+                |> List.map (\s -> "  " ++ s ++ "\n")
+                |> String.concat
+    , strong =
+        \s ->
+            String.concat
+                ("**" :: s ++ [ "**" ])
+    , emphasis =
+        \s ->
+            String.concat
+                ("*" :: s ++ [ "*" ])
+    , strikethrough =
+        \s ->
+            String.concat
+                ("~~" :: s ++ [ "~~" ])
+    , codeSpan =
+        \s ->
+            "`" ++ s ++ "`"
+    , link =
+        \link content ->
+            String.concat
+                [ "["
+                , String.concat content
+                , "]("
+                , link.destination
+                , ")"
+                ]
+    , image =
+        \imageInfo ->
+            String.concat
+                [ "!["
+                , imageInfo.alt
+                , "]("
+                , imageInfo.src
+                , ")"
+                ]
+    , text = identity
+    , unorderedList =
+        \items ->
+            items
+                |> List.map
+                    (\listitem ->
+                        case listitem of
+                            Block.ListItem Block.NoTask childs ->
+                                "- " ++ String.concat childs ++ "\n"
+
+                            Block.ListItem Block.IncompleteTask childs ->
+                                "- [ ]" ++ String.concat childs ++ "\n"
+
+                            Block.ListItem Block.CompletedTask childs ->
+                                "- [x]" ++ String.concat childs ++ "\n"
+                    )
+                |> String.concat
+    , orderedList =
+        \startingIndex items ->
+            items
+                |> List.indexedMap (\i item -> String.fromInt (i + startingIndex) ++ ") " ++ String.concat item ++ "\n")
+                |> String.concat
+    , html = Markdown.Html.oneOf []
+    , codeBlock =
+        \{ body, language } ->
+            String.concat
+                [ "```"
+                , language |> Maybe.withDefault ""
+                , "\n"
+                , body
+                , "```\n\n"
+                ]
+    , thematicBreak = "--------------------\n"
+
+    -- tables not currently parsed by elm-markdown.
+    , table = String.concat
+    , tableHeader = String.concat
+    , tableBody = String.concat
+    , tableRow = String.concat
+    , tableHeaderCell =
+        \maybeAlignment strs -> String.concat strs
+    , tableCell =
+        \maybeAlignment strs -> String.concat strs
+    }
+
+
 
 {-| Apply a `Renderer` to turn parsed `Markdown.Block`s into your rendered markdown view.
 -}
